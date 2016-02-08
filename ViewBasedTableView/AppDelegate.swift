aeca544c2eda60f354319190622b81ffb627799b
Bas van der Hoek <bvdhoek@zilverline.com>
2016-02-08 21:43:47 +0100

    Initial Commit

---
+++ ViewBasedTableView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ViewBasedTableView
+//
+//  Created by Bas van der Hoek on 08/02/16.
+//  Copyright © 2016 Bas van der Hoek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ ViewBasedTableView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViewBasedTableView
+//
+//  Created by Bas van der Hoek on 08/02/16.
+//  Copyright © 2016 Bas van der Hoek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
